2b06370580a5f17cd057a55eb0fdc91a845c843e
PedroGF27 <coolcatdopedog@gmail.com>
2025-03-24 18:31:39 -0700

    Add project files.

---
+++ COMP003B.LectureActivity2/Program.cs
@@ -0,0 +1,47 @@
+using COMP003B.LectureActivity2.Middleware;
+
+namespace COMP003B.LectureActivity2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+
+            app.UseStaticFiles();
+
+            app.UseMiddleware<COMP003B.LectureActivity2.Middleware.RequestLoggingMiddleware>();
+
+            app.UseWelcomePage("/Welcome!");
+
+			app.UseMiddleware<RequestLoggingMiddleware>()
+
+
+			app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
